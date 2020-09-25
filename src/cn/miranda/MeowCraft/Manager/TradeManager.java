63ffcb0e00df2c176abfc1808030ff0d28e80aef
MirandaMeow <vip444037570@gmail.com>
2020-09-25 10:24:54 +0800

    0.4.3 - Bug Fixed

--- src/cn/miranda/MeowCraft/Manager/TradeManager.java
+++
@@ -1,6 +0,0 @@
-package cn.miranda.MeowCraft.Manager;
-
-import java.io.Serializable;
-
-public class TradeManager implements Serializable {
-}
---
+++ src/cn/miranda/MeowCraft/Utils/IO.java
@@ -0,0 +1,30 @@
+package cn.miranda.MeowCraft.Utils;
+
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class IO {
+    public static String encodeData(Object object) throws IOException {
+        BASE64Encoder encoder = new BASE64Encoder();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BukkitObjectOutputStream objectOutputStream = new BukkitObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(object);
+        return encoder.encode(outputStream.toByteArray());
+    }
+
+    public static Object decodePlayerStatus(String cacheData) throws IOException, ClassNotFoundException {
+        BASE64Decoder decoder = new BASE64Decoder();
+        if (cacheData == null) {
+            return null;
+        }
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(decoder.decodeBuffer(cacheData));
+        BukkitObjectInputStream objectInputStream = new BukkitObjectInputStream(inputStream);
+        return objectInputStream.readObject();
+    }
+}
